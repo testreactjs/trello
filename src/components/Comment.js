0dbde9b41fa4fcfd5319d15bed0f67c7008ae620
purrweb <purrweb@iMac-purrweb.local>
2019-02-07 15:00:44 +0600

    new component Comment

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -7,38 +7,65 @@ class Comment extends React.Component {
     this.commentRef = React.createRef();
   }
 
-  handleHeaderClick = () => {
+  handleChange = () => {
     this.setState({ isClickedHeader: true }, () => {
       this.commentRef.current.focus();
     });
   };
 
-  handleHeaderSubmit = event => {
+  handleSubmit = event => {
     event.preventDefault();
     const text = this.commentRef.current.value;
-    // console.log(title);
-    this.setState({ isClickedHeader: false }, this.props.onChange(text));
+
+    if (text !== '') {
+      this.setState({ isClickedHeader: false }, this.props.onChange(text, this.commentRef.current.id));
+    }
   };
 
   render() {
     const { isClickedHeader } = this.state;
-    const { text, id } = this.props;
-    return isClickedHeader ? (
-      <div>
-        <textarea
-          ref={this.commentRef}
-          key={id}
-          className="form-control mt-2"
-          defaultValue={text}
-          onBlur={this.handleSaveEditedComment}
-        />
-        <button className="form-control btn btn-secondary" onClick={() => this.handleHeaderSubmit()}>
-          Save
+    const {
+      text,
+      id,
+      user: { avatar, firstName, surname },
+    } = this.props.comment;
+    const headComment = (
+      <section>
+        <span className="h3">
+          <img src={avatar} width="20" />
+          {firstName} {surname} :
+        </span>
+        <button className="float-right btn btn-info" onClick={() => this.props.onDelete(id)}>
+          Delete
         </button>
+        <button id={id} className="float-right btn btn-info mr-1" onClick={() => this.handleChange()}>
+          Edit
+        </button>
+      </section>
+    );
+    return isClickedHeader ? (
+      <div key={id} className="mt-3">
+        {headComment}
+        <div>
+          <textarea
+            id={id}
+            ref={this.commentRef}
+            key={id}
+            className="form-control mt-2"
+            defaultValue={text}
+            onBlur={this.handleSaveEditedComment}
+          />
+          <button className="form-control btn btn-secondary" onClick={() => this.handleSubmit(event)}>
+            Save
+          </button>
+        </div>
       </div>
     ) : (
-      <div key={id} className="form-control mt-2">
-        {text}
+      <div key={id} className="mt-3">
+        {headComment}
+        <div key={id} className="form-control mt-2">
+          {text}
+        </div>
       </div>
     );
   }
--- src/components/Comments.js
+++ src/components/Comments.js
@@ -2,50 +2,8 @@ import React from 'react';
 import Comment from './Comment';
 
 class Comments extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      id: '',
-    };
-    this.commentRef = React.createRef();
-  }
-
-  handleClickChangeComment = event => {
-    this.setState({ id: event.currentTarget.id });
-    // console.log(event.currentTarget.id);
-  };
-
-  handleSaveEditedComment = () => {
-    const text = this.commentRef.current.value;
-    if (text !== '') {
-      this.setState({ id: '' });
-      this.props.onEdit(text, this.commentRef.current.id);
-    }
-  };
-
-  // Show comment after map comments
   showComment = (comment, i) => {
-    const { id } = this.state;
-    const {
-      user: { avatar, firstName, surname },
-    } = comment;
-    // console.log('id this.state.id', typeof id, typeof this.state.id);
-
-    return (
-      <div key={comment.id} id={comment.id} className="mt-3">
-        <span className="h3">
-          <img src={avatar} width="20" />
-          {firstName} {surname} :
-        </span>
-        <button className="float-right btn btn-info" onClick={() => this.props.onDelete(comment.id)} id={comment.id}>
-          Delete
-        </button>
-        <button id={comment.id} className="float-right btn btn-info mr-1" onClick={this.handleClickChangeComment}>
-          Edit
-        </button>
-        <Comment comment={this.props.comment} onChange={}/>
-      </div>
-    );
+    return <Comment key={i} comment={comment} onChange={this.props.onEdit} onDelete={this.props.onDelete} />;
   };
 
   render() {
