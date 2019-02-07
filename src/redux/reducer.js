ea8fc477ce8e180b0ed4a3bbdb59e3cfd9a49a91
purrweb <purrweb@iMac-purrweb.local>
2019-02-07 18:29:35 +0600

    18 30

---
+++ src/redux/reducer.js
@@ -0,0 +1,77 @@
+import { createStore } from 'redux';
+import { createAction, createActions, handleActions, combineActions } from 'redux-actions';
+
+const testCard = [
+  {
+    listId: 7,
+    userId: 19,
+    id: 1,
+    title: 'voluptatem aliquid',
+    text: 'aliquam non inventore veritatis ut quis assumenda numquam illum labore',
+  },
+  {
+    listId: 1,
+    userId: 15,
+    id: 2,
+    title: 'debitis tempore',
+    text: 'sit ullam mollitia optio minima rerum amet laudantium blanditiis occaecati',
+  },
+];
+
+function handleCardAdd(title) {
+  console.log('handleCardAdd');
+  return {
+    listId: 2,
+    userId: 13,
+    id: 3,
+    title,
+    text: '',
+  };
+}
+
+const addCard = createAction('ADDCARD');
+const delCard = createAction('DELCARD');
+
+const defaultState = { cards: [...testCard] };
+console.log(defaultState);
+
+function handleDelCard(id) {
+  console.log('del');
+  return {};
+}
+
+const reducer = handleActions(
+  {
+    [addCard]: (state, action) => {
+      console.log('addCard');
+      const cards = () => handleCardAdd(state, action.payload);
+      return [...state, cards];
+    },
+    [delCard]: (state, { payload: { id } }) => {
+      return { ...state, cars: delCard(id) };
+    },
+  },
+  defaultState,
+);
+
+console.log(addCard('test new card'));
+console.log(defaultState);
+/*
+const defaultState = { counter: 10 };
+const { increment, decrement } = createActions({
+  INCREMENT: (amount = 1) => ({ amount }),
+  DECREMENT: (amount = 1) => ({ amount: -amount }),
+});
+
+const reducer = handleActions(
+  {
+    [combineActions(increment, decrement)]: (state, { payload: { amount } }) => {
+      return { ...state, counter: state.counter + amount };
+    },
+  },
+  defaultState,
+);
+*/
+
+console.log('the end redux..............................');
+export default { addCard, delCard };
