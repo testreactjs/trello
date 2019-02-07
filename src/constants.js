8ce1fc5075592e62633035dee6dfb94173c338b9
purrweb <purrweb@iMac-purrweb.local>
2019-02-07 12:50:29 +0600

    1300

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+const constants = {
+  ADD_CARD: 'ADD_CARD',
+  DEL_CARD: 'DEL_CARD',
+};
+/*
+export const card = (state = {}, action) => {
+  return {};
+};
+export const cards = (state = [], action) => {
+  return [];
+};
+*/
+export default constants;
